daac4541658e6019a1dbe732ea3172cea3ac307d
Wang Shihai <wangshihai@outlook.com>
2017-11-14 13:39:40 +0800

    clean up confiugration and add new window for extra game/server parameters

---
+++ Athena/Configuration.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace Athena
+{
+    public enum HMDTypes
+    {
+        NoHmd,
+        Default,
+        GoogleVR,
+        Occulus,
+        SteamVR
+    };
+
+    public class UserConfig
+    {
+        public bool with2017 = false;
+        public bool skipShaderCompiler = false;
+        public bool skipFrontend = false;
+        public bool skipLightmass = false;
+        public bool buildDebugGame = false;
+        public bool startServerDebug = false;
+        public bool startGameDebug = false;
+        public int gameInstances = 1;
+        public HMDTypes hmdType = HMDTypes.Default;
+        public RunParametersConfig gameParam = new RunParametersConfig();
+        public RunParametersConfig serverParam = new RunParametersConfig();
+    }
+
+    public class SharedConfig
+    {
+        public RunParametersConfig gameParam = new RunParametersConfig();
+        public RunParametersConfig serverParam = new RunParametersConfig();
+    }
+
+    public class AthenaConfig
+    {
+        static private string userConfigFileName = "athena_config.json";
+        static private string sharedConfigFileName = "athena_shared_config.json";
+        public UserConfig userConfig = new UserConfig();
+        public SharedConfig sharedConfig = new SharedConfig();
+
+        [ScriptIgnore]
+        public Dictionary<HMDTypes, string> HmdTypeStrings = new Dictionary<HMDTypes, string>
+        {
+            { HMDTypes.NoHmd, "-nohmd" },
+            { HMDTypes.Default, "" },
+            { HMDTypes.GoogleVR, "-hmd=SteamVR" },
+            { HMDTypes.Occulus, "-hmd=OculusRift" },
+            { HMDTypes.SteamVR, "-hmd=googlevrhmd" }
+        };
+
+        public static void Save(AthenaConfig config)
+        {
+            InternalSave<UserConfig>(userConfigFileName, config.userConfig);
+            InternalSave<SharedConfig>(sharedConfigFileName, config.sharedConfig);
+        }
+
+        public static void InternalSave<T>(string filename, T config)
+        {
+            var json = new JavaScriptSerializer().Serialize(config);
+            Console.WriteLine(json);
+
+            TextWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(filename);
+                writer.Write(json);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static AthenaConfig Load()
+        {
+            AthenaConfig config = new AthenaConfig();
+            config.userConfig = InternalLoad<UserConfig>(userConfigFileName);
+            config.sharedConfig = InternalLoad<SharedConfig>(sharedConfigFileName);
+
+            // make sure config is always valid, in case the config file has "null" in it
+            if (config.userConfig == null)
+            {
+                config.userConfig = new UserConfig();
+            }
+            if (config.sharedConfig == null)
+            {
+                config.sharedConfig = new SharedConfig();
+            }
+
+            return config;
+        }
+
+        public static T InternalLoad<T>(string filename) where T : new()
+        {
+            T config = new T();
+            if (File.Exists(filename))
+            {
+                TextReader reader = null;
+                try
+                {
+                    reader = new StreamReader(filename);
+                    var json = reader.ReadToEnd();
+                    config = new JavaScriptSerializer().Deserialize<T>(json);
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+
+            return config;
+        }
+    }
+
+    public class RunParametersConfig
+    {
+        public struct RunParameter
+        {
+            public int Id;
+            public string Param;
+        }
+
+        public List<RunParameter> Parameters = new List<RunParameter>();
+    }
+}
--- Athena/MainWindow.xaml.cs
+++ Athena/MainWindow.xaml.cs
@@ -13,45 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
-using System.Web.Script.Serialization;
 using System.IO;
 using System.Management;
 
 namespace Athena
 {
-    public class AthenaConfig
-    {
-        public enum HMDTypes
-        {
-            NoHmd,
-            Default,
-            GoogleVR,
-            Occulus,
-            SteamVR
-        };
-
-        static public string FileName = "athena_config.json";
-        public bool With2017 = false;
-        public bool SkipShaderCompiler = false;
-        public bool SkipFrontend = false;
-        public bool SkipLightmass = false;
-        public bool BuildDebugGame = false;
-        public bool StartServerDebug = false;
-        public bool StartGameDebug = false;
-        public int GameInstances = 1;
-        public HMDTypes HmdType = HMDTypes.Default;
-
-        [ScriptIgnore]
-        public Dictionary<HMDTypes, string> HmdTypeStrings = new Dictionary<HMDTypes, string>
-        {
-            { HMDTypes.NoHmd, "-nohmd" },
-            { HMDTypes.Default, "" },
-            { HMDTypes.GoogleVR, "-hmd=SteamVR" },
-            { HMDTypes.Occulus, "-hmd=OculusRift" },
-            { HMDTypes.SteamVR, "-hmd=googlevrhmd" }
-        };
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -60,36 +26,36 @@ namespace Athena
 
         public AthenaConfig config = new AthenaConfig();
         public Process GitAutoFetchProcess;
-        public Dictionary<AthenaConfig.HMDTypes, RadioButton> HmdTypes = new Dictionary<AthenaConfig.HMDTypes, RadioButton> { };
+        public Dictionary<HMDTypes, RadioButton> HmdTypes = new Dictionary<HMDTypes, RadioButton> { };
 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            HmdTypes.Add(AthenaConfig.HMDTypes.NoHmd, NoHmd);
-            HmdTypes.Add(AthenaConfig.HMDTypes.Default, DefaultHmd);
-            HmdTypes.Add(AthenaConfig.HMDTypes.GoogleVR, GoogleVR);
-            HmdTypes.Add(AthenaConfig.HMDTypes.Occulus, Occulus);
-            HmdTypes.Add(AthenaConfig.HMDTypes.SteamVR, SteamVR);
+            HmdTypes.Add(HMDTypes.NoHmd, NoHmd);
+            HmdTypes.Add(HMDTypes.Default, DefaultHmd);
+            HmdTypes.Add(HMDTypes.GoogleVR, GoogleVR);
+            HmdTypes.Add(HMDTypes.Occulus, Occulus);
+            HmdTypes.Add(HMDTypes.SteamVR, SteamVR);
 
             Load();
 
             // generate project
-            WithVS2017.IsChecked = config.With2017;
+            WithVS2017.IsChecked = config.userConfig.with2017;
 
             // build
-            BuildWithVS2017.IsChecked = config.With2017;
-            SkipShaderCompiler.IsChecked = config.SkipShaderCompiler;
-            SkipFrontend.IsChecked = config.SkipFrontend;
-            SkipLightmass.IsChecked = config.SkipLightmass;
-            DebugGame.IsChecked = config.BuildDebugGame;
+            BuildWithVS2017.IsChecked = config.userConfig.with2017;
+            SkipShaderCompiler.IsChecked = config.userConfig.skipShaderCompiler;
+            SkipFrontend.IsChecked = config.userConfig.skipFrontend;
+            SkipLightmass.IsChecked = config.userConfig.skipLightmass;
+            DebugGame.IsChecked = config.userConfig.buildDebugGame;
 
             // run
-            StartServerDebug.IsChecked = config.StartServerDebug;
-            StartGameDebug.IsChecked = config.StartGameDebug;
-            GameInstances.Text = config.GameInstances.ToString();
-            HmdTypes[config.HmdType].IsChecked = true;
+            StartServerDebug.IsChecked = config.userConfig.startServerDebug;
+            StartGameDebug.IsChecked = config.userConfig.startGameDebug;
+            GameInstances.Text = config.userConfig.gameInstances.ToString();
+            HmdTypes[config.userConfig.hmdType].IsChecked = true;
         }
 
         private Process ExecuteCommand(string command, bool createNoWindow = false, bool dontWaitForExit = false)
@@ -162,10 +128,10 @@ namespace Athena
 
         private void GenerateProjectButton_Click(object sender, RoutedEventArgs e)
         {
-            config.With2017 = WithVS2017.IsChecked == true;
+            config.userConfig.with2017 = WithVS2017.IsChecked == true;
             Save();
 
-            if (!config.With2017)
+            if (!config.userConfig.with2017)
             {
                 ExecuteCommand("GenerateProjectFiles.bat");
             }
@@ -177,31 +143,31 @@ namespace Athena
 
         private void BuildEditor_Click(object sender, RoutedEventArgs e)
         {
-            config.With2017 = BuildWithVS2017.IsChecked == true;
-            config.SkipShaderCompiler = SkipShaderCompiler.IsChecked == true;
-            config.SkipFrontend = SkipFrontend.IsChecked == true;
-            config.SkipLightmass = SkipLightmass.IsChecked == true;
-            config.BuildDebugGame = DebugGame.IsChecked == true;
+            config.userConfig.with2017 = BuildWithVS2017.IsChecked == true;
+            config.userConfig.skipShaderCompiler = SkipShaderCompiler.IsChecked == true;
+            config.userConfig.skipFrontend = SkipFrontend.IsChecked == true;
+            config.userConfig.skipLightmass = SkipLightmass.IsChecked == true;
+            config.userConfig.buildDebugGame = DebugGame.IsChecked == true;
             Save();
 
             string command = "BuildEditor.bat";
-            if (config.With2017)
+            if (config.userConfig.with2017)
             {
                 command += " -2017";
             }
-            if (config.SkipShaderCompiler)
+            if (config.userConfig.skipShaderCompiler)
             {
                 command += " --no-shader-compiler";
             }
-            if (config.SkipFrontend)
+            if (config.userConfig.skipFrontend)
             {
                 command += " --no-frontend";
             }
-            if (config.SkipLightmass)
+            if (config.userConfig.skipLightmass)
             {
                 command += " --no-lightmass";
             }
-            if (config.BuildDebugGame)
+            if (config.userConfig.buildDebugGame)
             {
                 command += " --debug-game";
             }
@@ -210,40 +176,40 @@ namespace Athena
 
         private void Run_Click(object sender, RoutedEventArgs e)
         {
-            config.StartGameDebug = StartGameDebug.IsChecked == true;
-            config.GameInstances = 1;
-            Int32.TryParse(GameInstances.Text, out config.GameInstances);
-            config.HmdType = AthenaConfig.HMDTypes.Default;
-            foreach (KeyValuePair<AthenaConfig.HMDTypes, RadioButton> entry in HmdTypes)
+            config.userConfig.startGameDebug = StartGameDebug.IsChecked == true;
+            config.userConfig.gameInstances = 1;
+            Int32.TryParse(GameInstances.Text, out config.userConfig.gameInstances);
+            config.userConfig.hmdType = HMDTypes.Default;
+            foreach (KeyValuePair<HMDTypes, RadioButton> entry in HmdTypes)
             {
                 if (entry.Value.IsChecked == true)
                 {
-                    config.HmdType = entry.Key;
+                    config.userConfig.hmdType = entry.Key;
                     break;
                 }
             }
             Save();
             
-            for (int i = 0; i < config.GameInstances; ++i)
+            for (int i = 0; i < config.userConfig.gameInstances; ++i)
             {
                 string command = "RunAssociatedEngine.cmd -log -game -resx=1280 -resy=720 -windowed";
-                if (config.StartGameDebug)
+                if (config.userConfig.startGameDebug)
                 {
                     command += " -debug";
                 }
                 command += " ";
-                command += config.HmdTypeStrings[config.HmdType];
+                command += config.HmdTypeStrings[config.userConfig.hmdType];
                 ExecuteCommand(command, true);
             }
         }
 
         private void RunServer_Click(object sender, RoutedEventArgs e)
         {
-            config.StartServerDebug = StartServerDebug.IsChecked == true;
+            config.userConfig.startServerDebug = StartServerDebug.IsChecked == true;
             Save();
 
             string command = "RunAssociatedEngine.cmd -log -server";
-            if (config.StartGameDebug)
+            if (config.userConfig.startGameDebug)
             {
                 command += " -debug";
             }
@@ -252,41 +218,12 @@ namespace Athena
 
         private void Save()
         {
-            var json = new JavaScriptSerializer().Serialize(config);
-            Console.WriteLine(json);
-
-            TextWriter writer = null;
-            try
-            {
-                writer = new StreamWriter(AthenaConfig.FileName);
-                writer.Write(json);
-            }
-            finally
-            {
-                if (writer != null)
-                    writer.Close();
-            }
+            AthenaConfig.Save(config);
         }
 
         private void Load()
         {
-            if (File.Exists(AthenaConfig.FileName))
-            {
-                TextReader reader = null;
-                try
-                {
-                    reader = new StreamReader(AthenaConfig.FileName);
-                    var json = reader.ReadToEnd();
-                    config = new JavaScriptSerializer().Deserialize<AthenaConfig>(json);
-                }
-                finally
-                {
-                    if (reader != null)
-                    {
-                        reader.Close();
-                    }
-                }
-            }
+            config = AthenaConfig.Load();
         }
 
         private void WithVS2017_Checked(object sender, RoutedEventArgs e)
@@ -368,6 +305,12 @@ namespace Athena
                     KillProcessAndChildrens(Convert.ToInt32(mo["ProcessID"])); //kill child processes(also kills childrens of childrens etc.)
                 }
             }
+        }
+
+        private void ServerParameters_Click(object sender, RoutedEventArgs e)
+        {
+            ParameterWindow parameterWindow = new ParameterWindow();
+            parameterWindow.ShowDialog();
         }
     }
 
